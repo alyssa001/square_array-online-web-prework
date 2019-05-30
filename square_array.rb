1c228799435ff01c7f8041a92d46186d6f5600a3
Alyssa Yesilyurt <alyssa.yesilyurt@gmail.com>
2019-05-30 20:03:15 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,10 +1,7 @@
 def square_array(array)
-  empty_arr =[ ]
+  square_arr = [ ]
   array.each do |num|
-    array.push(num**2)
+    arr << num**2
   end
-end
-
-  def collector
-    array.collect{|num| num **2}
+  arr
 end
